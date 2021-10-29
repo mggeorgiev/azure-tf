07510d5c38abe346ca643a87d9a148775676a92d
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2021-10-29 15:54:14 +0200

    Update main.tf

--- modules/storage-account-module/main.tf
+++ modules/storage-account-module/main.tf
@@ -8,7 +8,7 @@ locals {
 resource "azurerm_storage_account" "storageaccount" {
     name                        = var.storage_account_name
     resource_group_name         = var.resource_group
-    location                    = var.stolocation
+    location                    = var.sto_location
     account_tier                = var.account_tier
     account_replication_type    = var.account_replication_type
 
