444b3c1bc3ee22bbae3b726d1a63ba575f2eddaf
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2021-10-29 13:35:41 +0200

    Update variables.tf

--- modules/storage-account-module/variables.tf
+++ modules/storage-account-module/variables.tf
@@ -8,23 +8,22 @@ variable sto_name {
     validation {
         condition     = length(var.sto_name) < 3 && length(var.sto_name) > 24
         error_message = "The storage account name must be between 3 and 24 symbols."
-  }
+    }
 
     validation {
         condition     = lower(var.sto_name) != var.sto_name
         error_message = "The storage account name lowercase characters"
-  }
-
+    }
 
     validation {
         condition     = regex([a-z0-9], var.sto_name) != var.sto_name
         error_message = "The storage account name lowercase characters"
-  }
+    }
 
-      validation {
-        condition     = substr(var.sto_name, 0, 3) == "sto"
+    validation {
+        condition     = substr(var.sto_name, 0, 3) != "sto"
         error_message = "The storage account name must start with sto"
-  }
+    }
 }
 
 variable tier {
