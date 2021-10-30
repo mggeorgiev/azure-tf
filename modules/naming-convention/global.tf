a85825aa60075a2ed3271b3a896066b46be08d3c
Martin Georgiev <mggeorgiev@gmail.com>
2021-10-30 09:24:23 +0200

    add global

--- modules/naming-convention/global.tf
+++ modules/naming-convention/global.tf
@@ -9,3 +9,27 @@ variable "resource_group_name" {
     type        = string
     description = "rg-<app or service name>-<subscription type>-<###>"
 }
+
+variable "regions" {
+    type = map
+    default = {
+        "Global"        = "global"
+        "Central US"    = "centralus"
+        "East US"       = "eastus"
+        "East US 2"     = "eastus2"
+        "West Europe"   = "westeurope"
+        "North Europe"  = "northeurope"
+    }
+}
+
+variable "regions_prefixes" {
+    type = map
+    default = {
+        "global"        = "glob"
+        "centralus"     = "cus1"
+        "eastus"        = "eus1"
+        "eastus2"       = "eus2"
+        "westeurope"    = "weu1"
+        "northeurope"   = "neu1"
+    }
+}
